b740a3920ced26a3bb9046e9b7e9c1a0327f8310
dbjdbj <dbj@dbj.org>
2021-07-06 11:25:12 +0200

    folders reshuffle

--- build.cmd
+++ build.cmd
@@ -1,10 +1,23 @@
+@rem
+@rem
+@rem primary use of this is VCode building and debugging
+@rem for people who do not want VStudio
+@rem
+@rem this build outputs to the same  
+@rem folder as VStudio solution does
+@rem so if changinf the name out/test.exe 
+@rem be carefull not to hit the same
+@rem exe names as VStudio solution uses 
+@rem
+@rem dbj_matmul_Release.exe and dbj_matmul_Debug.exe
 @cls
 
 @if [%1] == [clean] goto clean
 
 :build
 @rem obviously you know /Zi makes it a debug build
-clang-cl /openmp /TC /Zi main.c /o out/test.exe
+@rem and you know /MDd is dll runtime lib debug version
+clang-cl /openmp /TC /MDd /Zi main.c /o out/test.exe
 @goto exit
 
 :clean
